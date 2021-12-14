e21440ef33bd8ba5eefb8de6f49148ba43422299
Jose Garcia <rocketrito1@gmail.com>
2021-12-14 13:06:47 -0800

    commit tree c6dbd5bc53fd3e1402f4a10b3d73c8eb622c2f20

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(x) {
+    return Math.abs(42 - x);
+}
+
+function distanceFromHqInFeet(x) {
+    return distanceFromHqInBlocks(x) * 264;
+}
+
+function distanceTravelledInFeet(x,y) {
+    return Math.abs((x-y)*264);
+}
+
+function calculatesFarePrice(x,y){
+    if (distanceTravelledInFeet(x,y) <= 400)
+    return 0;
+  
+    else if (distanceTravelledInFeet(x,y) > 400 && distanceTravelledInFeet(x,y) <= 2000)
+    return ((distanceTravelledInFeet(x,y)-400)*2)*.01;
+  
+    else if (distanceTravelledInFeet(x,y) > 2000 && distanceTravelledInFeet(x,y) <= 2500)
+    return 25;
+  
+    //not at all how i assumed the && operator worked
+  
+    else if (distanceTravelledInFeet(x,y) > 2500)
+    return 'cannot travel that far';
+  }
